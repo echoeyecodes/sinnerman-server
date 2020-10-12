4d39511c4264b4ed269f614ead143322f6d32f82
echoeyecodes <femiobajuluwa@gmail.com>
2020-10-12 13:07:09 +0100

    updated pubsub credentials to env variables

--- utils/pubusbSingleton.ts
+++ utils/pubusbSingleton.ts
@@ -3,9 +3,6 @@ import { PubSub } from "@google-cloud/pubsub";
 import config from "../pubsub-config.json";
 
 export default (function generatePubSub() {
-  const pubsub = new PubSub({
-    credentials: config,
-    projectId: config.project_id,
-  });
+  const pubsub = new PubSub();
   return pubsub;
 })()
