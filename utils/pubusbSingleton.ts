2651959cd2b444f602e194a58bb04045c9bc9089
echoeyecodes <femiobajuluwa@gmail.com>
2020-10-13 08:39:35 +0100

    testing manual pubsub config

--- utils/pubusbSingleton.ts
+++ utils/pubusbSingleton.ts
@@ -3,6 +3,6 @@ import { PubSub } from "@google-cloud/pubsub";
 import config from "../pubsub-config.json";
 
 export default (function generatePubSub() {
-  const pubsub = new PubSub();
+  const pubsub = new PubSub({projectId: process.env.GOOGLE_CRED_PROJECT_ID, credentials:{private_key: process.env.GOOGLE_CRED_PRIVATE_KEY, client_email: process.env.GOOGLE_CRED_CLIENT_EMAIL}, clientOptions:{clientId: process.env.GOOGLE_CRED_CLIENT_ID, email:process.env.GOOGLE_CRED_CLIENT_EMAIL}});
   return pubsub;
 })()
