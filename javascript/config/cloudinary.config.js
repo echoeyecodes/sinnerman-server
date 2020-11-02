adb55cb4316b38e3835f1ae8e81164ba1d94d03b
echoeyecodes <femiobajuluwa@gmail.com>
2020-11-02 15:39:55 +0100

    typescript sources only

--- javascript/config/cloudinary.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET_KEY,
-};
--- javascript/utils/pubusbSingleton.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pubsub_1 = require("@google-cloud/pubsub");
-exports.default = (function generatePubSub() {
-    const pubsub = new pubsub_1.PubSub();
-    return pubsub;
-})();
